3981110ab853749a7c37ba472cc62e5d6042d056
Carlos Estrada <cestr008@gmail.com>
2023-04-16 11:25:01 -0400

    useState exercise 4

--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,37 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';import {data} from '../../../data'
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [people, setPeople] = useState([]);
+  const HOBBIES = ['Gardening', 'Coding', 'Reading', 'Writing', 'Hiking', 'Biking', 'Swimming', 'Skiing', 'Sailing', 'Climbing', 'Fishing', 'Hunting', 'Cooking', 'Dancing', 'Singing', 'Acting', 'Playing', 'Watching', 'Listening', 'Eating', 'Sleeping', 'Shopping', 'Traveling', 'Working', 'Studying', 'Teaching', 'Learning'];
+  const [pIndex, setPIndex] = useState(0);
+
+  useEffect(() => {
+    setPeople(data.map((person) => {
+      const newAttributes =  {age: parseInt(Math.random() * 99), hobby: HOBBIES[parseInt(Math.random() * HOBBIES.length)]};
+      return {...person, ...newAttributes}
+    }));
+
+  }, []);
+
+  const handleSwitchPerson = () => {
+    const i = (pIndex + 1) % people.length;
+    setPIndex(i)
+  }
+
+
+  return <>
+  <h2>
+    <p>{pIndex}</p>
+    {people.length > 0 && (
+      <div key={people[pIndex].id}>
+        <h3>{people[pIndex].name}</h3>
+        <p>{people[pIndex].age}</p>
+        <p>{people[pIndex].hobby}</p>
+      </div>
+      )}
+  </h2>
+  <button onClick={handleSwitchPerson}>Show Next Person</button>
+  </>
 };
 
 export default UseStateObject;
